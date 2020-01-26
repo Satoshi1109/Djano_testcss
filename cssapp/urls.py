8286593454ad2ea11c345ad05422131cdd402a9e
User Name <user@example.com>
2020-01-26 14:04:27 +0900

    Signup画面作成

--- cssapp/urls.py
+++ cssapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, signupfunc, loginfunc
 
 urlpatterns = [
     path('', index, name='index'),
+    path('signup/', signupfunc, name='signup'),
+    path('login/',loginfunc, name='login'),
 ]
--- cssapp/views.py
+++ cssapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.contrib.auth.models import User
 
 def index(request):
     myapp_data = {
@@ -7,3 +8,17 @@ def index(request):
     'num': range(10),
     }
     return render(request, 'index.html', myapp_data)
+
+def signupfunc(request):
+    if request.method == 'POST':
+        new_username = request.POST['username']
+        new_password = request.POST['password']
+        try:
+            User.objects.get(username = new_username)
+            return render(request, 'signup.html', {'error':'このユーザーは既に登録されています。'})
+        except User.DoesNotExist:
+            user = User.objects.create_user(new_username, '', new_password)
+            return render(request, 'signup.html')
+    else :
+        print('NOT POST')
+    return render(request, 'signup.html')
